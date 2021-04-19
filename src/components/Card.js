63efd3014c162df7e658ae9d4f1cca86f0325b63
Olugbenga Ajayi <gbejula@gmail.com>
2021-04-19 04:44:05 +0100

    static react files

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,9 @@
+import './Card.css';
+
 const Card = props => {
-  return <div className='card'>{props.children}</div>;
+  const classes = 'card ' + props.className;
+
+  return <div className={classes}>{props.children}</div>;
 };
 
 export default Card;
